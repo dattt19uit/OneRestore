5bc0cab237efeb3a1997099627c8d1a0653ff80f
Hao Hoang <haohoangofficial@gmail.com>
2025-09-19 23:29:14 +0700

    update

--- train_Embedder.py
+++ train_Embedder.py
@@ -27,6 +27,7 @@ def train_embedding(cur_epoch, model, optimizer, trainloader, testloader, device
 
         model.train()
         for idx, batch in enumerate(trainloader):
+            print(batch)
             for i in range(len(batch)):
                 batch[i] = batch[i].to("cuda" if torch.cuda.is_available() else "cpu")
             time_start = time.time()

9dfafcbf1f471bef20389a02ae132419a2dc816d
Hao Hoang <haohoangofficial@gmail.com>
2025-09-20 00:38:07 +0700

    update

--- model/Embedder.py
+++ model/Embedder.py
@@ -264,7 +264,7 @@ class Embedder(nn.Module):
     def forward(self, batch, mode='train'):
         images, captions = batch
         print(images)
-        img_emb = self.encode_image(images)
+        img_emb = self.image_encoder_forward(images)
         return
         txt_emb = self.encode_text(captions)
 
